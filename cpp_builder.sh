13121643d71a7596b145ecb6b8199ebe25c64ba7
Tao Xiang <xiang.tao@outlook.de>
2024-08-09 09:52:59 +0800

    1

---
+++ cpp_builder.sh
@@ -0,0 +1,32 @@
+# Prompt user for Python and pip paths
+echo "请输入Python路径:"
+read python_path
+
+echo "请输入pip路径:"
+read pip_path
+
+# Install python environments
+echo "安装python环境 ..."
+$pip_path install -r requirements.txt
+
+echo ""
+echo ""
+
+echo "导出yolov8s onnx ..."
+$python_path export-det.py \
+--weights yolov8s.pt \
+--iou-thres 0.65 \
+--conf-thres 0.25 \
+--topk 100 \
+--opset 11 \
+--sim \
+--input-shape 1 3 640 640 \
+--device cuda:0
+
+echo ""
+echo ""
+
+echo "转为enginefile ..."
+/usr/local/TensorRT/bin/trtexec --onnx=yolov8s.onnx --saveEngine=yolov8s.engine --fp16
+echo ""
+echo ""
